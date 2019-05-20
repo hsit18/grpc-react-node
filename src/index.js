6ccd0d50410b1923d66a28f91c8dcd576b4575a3
harpreet singh <hsit18@gmail.com>
2019-05-20 12:05:30 +0530

    styling hello app

--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const { HelloRequest, HelloReply } = require("./proto/hello_pb.js");
-const { GreeterClient } = require("./proto/hello_grpc_web_pb.js");
-
-const callHelloGrpcService = () => {
-  const client = new GreeterClient("http://localhost:9090");
-  const request = new HelloRequest();
-  request.setName("World");
-
-  client.sayHello(request, {}, (err, response) => {
-    console.log(response.getMessage());
-  });
-};
+import HelloApp from "./HelloApp";
 
 const App = () => (
-  <div>
-    Testing Hello World grpc
-    <button style={{ padding: 10 }} onClick={callHelloGrpcService}>
-      Click for grpc request
-    </button>
+  <div className="container justify-content-md-center">
+    <HelloApp />
   </div>
 );
 
